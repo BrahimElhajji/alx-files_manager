011fba334337fdf2788a39ba20969b9b158756b1
BrahimElhajji <elhajji080898@gmail.com>
2024-10-03 02:21:14 +0000

    test

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
+import FilesController from '../controllers/FilesController'; // Import FilesController
 
 const router = Router();
 
@@ -15,11 +15,14 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
-router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
 
-// Add new routes for publishing/unpublishing
+// New route for file upload
+router.post('/files', FilesController.postUpload);
+
+// New routes for getting files
+router.get('/files/:id', FilesController.getShow); // GET file by id
+router.get('/files', FilesController.getIndex); // GET all files with pagination
+
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
